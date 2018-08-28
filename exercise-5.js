7f8a85e3b3682a6c063755fec4dc622765a4f2f0
Muhammad <accank72@gmail.com>
2018-08-28 22:53:54 +0800

    revisi exercise-5.js

--- exercise-5.js
+++ exercise-5.js
@@ -1,27 +1,10 @@
 function attack (damage) {
-  // Code disini
-  //var damageReduction = 2;
   var attackFunction = damage - 2;
-  
   return attackFunction;
 }
 
 function damageCalculation (numberOfAttacks, damagePerAttack) {
-  // Code disini
   
-   //Cara rekursif
-  /*var attacksRemaining = numberOfAttacks;
-  
-  if (numberOfAttacks === 0) {
-    return 1;
-  } else if (attacksRemaining < numberOfAttacks) {
-    attacksRemaining -= 1;
-  }
-  
-  return attacksRemaining * attack(damagePerAttack);*/
-  
-  
-  // Cara biasa
   var attackFunction = attack(damagePerAttack);
   var numberofDamage = attackFunction * numberOfAttacks;
   
@@ -29,19 +12,6 @@ function damageCalculation (numberOfAttacks, damagePerAttack) {
 }
 
 
-/*
-// // Exercise 4 Week 5 Case 'Damage Calculation'
-// by Muhamad Haddawi Hermawan Hacktiv8 Student Phase 0 Batch QFox
-
-function attack (damage) {
-    return damage - 2;
-}
-
-function damageCalculation (numberOfAttacks, damagePerAttack) {
-    return attack(damagePerAttack) * numberOfAttacks;
-}
-
-*/
 
 // TEST CASE
 console.log(damageCalculation(9, 25)); // 207
